c6fc4657bbe4efd512a66390689d44da38855d9c
soenneker <jake@soenneker.com>
2026-08-29 14:17:06 -0500

    Improve public API XML documentation

--- src/Soenneker.Groq.HttpClients/Abstract/IGroqOpenApiHttpClient.cs
+++ src/Soenneker.Groq.HttpClients/Abstract/IGroqOpenApiHttpClient.cs
@@ -2,7 +2,6 @@ using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Threading;
-
 namespace Soenneker.Groq.HttpClients.Abstract;
 
 /// <summary>
@@ -10,5 +9,10 @@ namespace Soenneker.Groq.HttpClients.Abstract;
 /// </summary>
 public interface IGroqOpenApiHttpClient: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Returns the configured HTTP client used by the Groq OpenAPI HTTP Client.
+    /// </summary>
+    /// <param name="cancellationToken">Token used to cancel the operation.</param>
+    /// <returns>A task whose result is the requested HTTP client.</returns>
     ValueTask<HttpClient> Get(CancellationToken cancellationToken = default);
 }
--- src/Soenneker.Groq.HttpClients/Registrars/GroqOpenApiHttpClientRegistrar.cs
+++ src/Soenneker.Groq.HttpClients/Registrars/GroqOpenApiHttpClientRegistrar.cs
@@ -13,6 +13,8 @@ public static class GroqOpenApiHttpClientRegistrar
     /// <summary>
     /// Adds <see cref="GroqOpenApiHttpClient"/> as a singleton service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddGroqOpenApiHttpClientAsSingleton(this IServiceCollection services)
     {
         services.AddHttpClientCacheAsSingleton()
@@ -24,6 +26,8 @@ public static class GroqOpenApiHttpClientRegistrar
     /// <summary>
     /// Adds <see cref="GroqOpenApiHttpClient"/> as a scoped service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddGroqOpenApiHttpClientAsScoped(this IServiceCollection services)
     {
         services.AddHttpClientCacheAsSingleton()
